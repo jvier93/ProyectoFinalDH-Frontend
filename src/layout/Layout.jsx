f2065774b781b6c475575feafe10ad2dddfdbe12
Pablo Kraiñuk <76402106+jvier93@users.noreply.github.com>
2024-08-06 14:37:03 -0300

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "@/components/Footer";
 
 const Layout = () => {
   return (
     <div className="flex h-[2000px] flex-col ">
-      <Navbar />
+      <Header />
       <div className="flex-grow">
         <Outlet />
       </div>
